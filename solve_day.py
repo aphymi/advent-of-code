33987adbed557f9ecb837204f3ed1f1ccc0bfc26
aphymi <aphymi@gmail.com>
2018-12-03 19:57:14 -0500

    Retrieve (and save) input from server if input file not found.

--- solve_day.py
+++ solve_day.py
@@ -4,6 +4,38 @@ import argparse
 from importlib import import_module
 import sys
 
+import requests
+
+def retrieve_day_input(day, day_name):
+	
+	try:
+		with open("inputs/" + day_name + ".txt") as file:
+			input_file = file.read()
+	
+	except FileNotFoundError:
+		print("No input file found; retrieving input remotely.")
+		try:
+			with open(".session") as file:
+				sess = file.read().strip()
+
+		except FileNotFoundError:
+			print("Error: Neither saved input file nor .session cookie file could be found.")
+			sys.exit(1)
+		
+		r = requests.get("https://adventofcode.com/2018/day/{}/input".format(day),
+							cookies={"session": sess})
+
+		if r.status_code != 200:
+			print("Error: Unable to retrieve input file.")
+			sys.exit(1)
+		
+		input_file = r.text
+
+		with open("inputs/" + day_name + ".txt", "w") as file:
+			file.write(input_file)
+
+	return input_file.splitlines()
+
 if __name__ == "__main__":
 	
 	parser = argparse.ArgumentParser(description="Solve a day of the advent of code.")
@@ -21,19 +53,13 @@ if __name__ == "__main__":
 	day_name = "day{:02d}".format(args.day)
 	
 	try:
-		with open("inputs/" + day_name + ".txt") as file:
-			split_input = file.read().split("\n")
-	
-	except FileNotFoundError:
-		print("Error: No input file found for {}.".format(day_name))
-		sys.exit(1)
-	
-	try:
 		day_mod = import_module("sols." + day_name)
 	
 	except ImportError:
 		print("Error: No solution module found for {}.".format(day_name))
 		sys.exit(1)
+	
+	input_lines = retrieve_day_input(args.day, day_name)
 	
 	parts_to_eval = ("1", "2") if args.part is None else (args.part,)
 	
@@ -42,4 +68,4 @@ if __name__ == "__main__":
 		if not hasattr(day_mod, part_name):
 			print("Error: Solution for day {} has no function {}.".format(args.day, part_name))
 		else:
-			print("Part {} answer: {}".format(part, getattr(day_mod, part_name)(split_input)))
+			print("Part {} answer: {}".format(part, getattr(day_mod, part_name)(input_lines)))
