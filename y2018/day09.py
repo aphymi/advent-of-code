e6b148348cc3f0cb6c08534b396702ebc8904e88
aphymi <aphymi@gmail.com>
2018-12-10 15:10:12 -0500

    Day 9 optimisations.

--- y2018/day09.py
+++ y2018/day09.py
@@ -1,36 +1,33 @@
+from collections import deque
 import re
 
 def preprocess_input(lines):
-	return map(int, re.findall("\d+", lines[0]))
+	return list(map(int, re.findall("\d+", lines[0])))
 
-def part1(stats):
-	num_players, last_marble = stats
-
+def max_marble_score(num_players, last_marble):
 	player_scores = [0]*num_players
-	marbles = [0]
+	marbles = deque([0])
 
 	marble_num = 0
-	last_ind = 0
 
 	while True:
 		for player in range(len(player_scores)):
 			marble_num += 1
 			if marble_num % 23 == 0:
-				omarble_ind = (last_ind - 7) % len(marbles)
-				player_scores[player] += marble_num + marbles[omarble_ind]
-
-				del marbles[omarble_ind]
-				last_ind = omarble_ind
+				marbles.rotate(7)
+				player_scores[player] += marble_num + marbles.popleft()
 			
 			else:
-				last_ind = (last_ind + 2) % len(marbles)
-				marbles.insert(last_ind, marble_num)
+				marbles.rotate(-2)
+				marbles.appendleft(marble_num)
 
 			if marble_num >= last_marble:
 				return max(player_scores)
-				
 
+def part1(stats):
+	return max_marble_score(*stats)
+	
 def part2(stats):
 	num_players, last_marble = stats
-	return part1((num_players, last_marble*100))
+	return max_marble_score(num_players, last_marble*100)
 
