a35bd70fe0154fff8521a4d3f3d72e7cb5715233
aphymi <aphymi@gmail.com>
2018-12-26 21:41:24 -0500

    Y2015 Day 9 refactoring.

--- y2015/day09.py
+++ y2015/day09.py
@@ -1,32 +1,29 @@
-def preprocess_input(lines):
-	routes = []
-	for line in lines:
-		l1, rest = line.split(" to ")
-		l2, dist = rest.split(" = ")
-		routes.append((l1, l2, int(dist)))
-	return routes
+from itertools import permutations
 
-def part1(routes):
+def preprocess_input(lines):
+	dists = {}
+	for line in lines:
+		line = line.split()
+		dists[(line[0], line[2])] = int(line[4])
+		dists[(line[2], line[0])] = int(line[4])
+		
+	return dists
+
+def part1(dists):
 	def shortest_route_from(rts, start): # Greedy!
 		s = 0
 		while rts:
-			l1, l2, d = max([(l1, l2, d) for l1, l2, d in rts if start in (l1, l2)], key=lambda r: r[2])
-			s += d
-			rts = [(l1, l2, d) for l1, l2, d in rts if start not in (l1, l2)]
-			start = [l for l in (l1, l2) if l != start][0]
+			min_route = min((ls for ls in rts if ls[0] == start), key=lambda ls: rts[ls])
+			s += rts[min_route]
+			rts = {ls: d for ls, d in rts.items() if start not in ls}
+			start = min_route[1]
 		
 		return s
 	
-	return max([shortest_route_from(routes, strt) for strt in set([rt[0] for rt in routes] + [rt[1] for rt in routes])])
+	return min(shortest_route_from(dists, loc) for loc in set(l1 for l1, l2 in dists))
 
-def part2(routes):
-	def longest_route_from(rts, start): # Exponential!
-		if not rts:
-			return 0
-		
-		next_rts = [([l for l in (l1, l2) if l != start][0], d) for l1, l2, d in rts if start in (l1, l2)]
-		
-		return max([d + longest_route_from([(l1, l2, d_) for (l1, l2, d_) in rts if start not in (l1, l2)], l)
-							for l, d in next_rts])
+def part2(dists):
+	def route_len(dists, route):
+		return sum(dists[path] for path in zip(route[:-1], route[1:]))
 	
-	return max([longest_route_from(routes, strt) for strt in set([rt[0] for rt in routes] + [rt[1] for rt in routes])])
+	return max(route_len(dists, route) for route in permutations(set(l1 for l1, l2 in dists)))
