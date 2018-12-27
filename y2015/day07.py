d0a5554a373b665c4d1bfb8bd75a655305522b9c
aphymi <aphymi@gmail.com>
2018-12-26 21:00:46 -0500

    Y2015 Day 7 refactoring.

--- y2015/day07.py
+++ y2015/day07.py
@@ -1,42 +1,35 @@
-from pprint import pprint
-import re
-
 import numpy as np
 
 def preprocess_input(lines):
 	rules = {}
 	for line in lines:
 		inp, out = line.split(" -> ")
+		inp = inp.split()
 		
-		op = re.findall("[A-Z]+", inp)
-		if not op:
-			op = "ID"
-		else:
-			op = op[0]
+		if len(inp) == 1:
+			rules[out] = {"op": None, "args": inp}
+			
+		elif len(inp) == 2:
+			rules[out] = {"op": inp[0], "args": inp[1:]}
 		
-		args = re.findall("[a-z]+|\d+", inp)
-		
-		rules[out] = {
-			"op": op,
-			"args": args,
-		}
+		else: # len(inp) == 3
+			rules[out] = {"op": inp[1], "args": (inp[0], inp[2])}
 		
 	return rules
 
+wire_cache = {}
 def get_value(rules, wire):
+	if wire in wire_cache:
+		return wire_cache[wire]
+	
 	if wire.isdigit():
 		return np.uint16(int(wire))
 	
 	rule = rules[wire]
 	
-	if "val" in rule:
-		return rule["val"]
-	
 	op, args = rule["op"], rule["args"]
 	
-	if op == "ID":
-		val = get_value(rules, args[0])
-	elif op == "NOT":
+	if op == "NOT":
 		val = ~ get_value(rules, args[0])
 	elif op == "OR":
 		val = get_value(rules, args[0]) | get_value(rules, args[1])
@@ -46,23 +39,24 @@ def get_value(rules, wire):
 		val = get_value(rules, args[0]) >> get_value(rules, args[1])
 	elif op == "LSHIFT":
 		val = get_value(rules, args[0]) << get_value(rules, args[1])
-	else:
-		raise Exception
+	else: # op == None
+		val = get_value(rules, args[0])
 	
-	rule["val"] = val
+	wire_cache[wire] = val
 	return val
 	
 
 def part1(rules):
-	return get_value(rules, "a")
+	try:
+		return get_value(rules, "a")
+	finally:
+		wire_cache.clear()
 
 def part2(rules):
-	a = get_value(rules, "a")
-	for rule in rules.values():
-		if "val" in rule:
-			del rule["val"]
-	
-	rules["b"]["op"] = "ID"
-	rules["b"]["args"] = (str(a),)
-	
-	return get_value(rules, "a")
+	try:
+		a = get_value(rules, "a")
+		wire_cache.clear()
+		wire_cache["b"] = a
+		return get_value(rules, "a")
+	finally:
+		wire_cache.clear()
