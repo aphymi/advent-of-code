3f3ddf75bf0cb32f0e6ba470e40a68685023f8ae
aphymi <aphymi@gmail.com>
2018-12-26 22:19:55 -0500

    Y2015 Day 13 refactoring.

--- y2015/day13.py
+++ y2015/day13.py
@@ -2,33 +2,20 @@ from collections import defaultdict
 from itertools import permutations
 
 def preprocess_input(lines):
-	prefs = defaultdict(dict)
+	prefs = defaultdict(int)
 	for line in lines:
 		line = line.split()
-		fst = line[0][0]
-		snd = line[-1][0]
-		
-		prefs[fst][snd] = int(line[3]) * (-1 if line[2] == "lose" else 1)
+		prefs[(line[0], line[-1][:-1])] = int(line[3]) * (1 if line[2] == "gain" else -1)
 	
 	return prefs
 
-def score(prefs, arr):
-	s = prefs[arr[0]][arr[-1]] + prefs[arr[-1]][arr[0]]
-	for i in range(len(arr)-1):
-		s += prefs[arr[i]][arr[i+1]]
-	for i in range(1, len(arr)):
-		s += prefs[arr[i]][arr[i-1]]
-	return s
+def score(prefs, arrange):
+	return sum(prefs[(a1, a2)] + prefs[(a2, a1)] for a1, a2 in zip(arrange[:-1], arrange[1:])) + \
+				prefs[(arrange[-1], arrange[0])] + prefs[(arrange[0], arrange[-1])]
 
 def part1(prefs):
-	return score(prefs, max(permutations(prefs.keys()), key=lambda arr: score(prefs, arr)))
+	return max(score(prefs, arrange) for arrange in permutations(set(p1 for p1, p2 in prefs)))
 
 def part2(prefs):
 	prefs = prefs.copy()
-	# noinspection PyStatementEffect
-	prefs["_"] # Have to just mention it, to get it created so that the dict size doesn't change during iteration.
-	for key in prefs:
-		prefs[key]["_"] = 0
-		prefs["_"][key] = 0
-	
-	return score(prefs, max(permutations(prefs.keys()), key=lambda arr: score(prefs, arr)))
+	return max(score(prefs, arrange) for arrange in permutations(set(p1 for p1, p2 in prefs) | {"_"}))
