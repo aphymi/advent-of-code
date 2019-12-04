d4cb75fba3f4d985d0c2c85932e9346c19a6b085
aphymi <aphymi@gmail.com>
2019-12-03 20:22:47 -0500

    Rework input retrieval, and refactor solve_day.py

---
+++ input_retrieval.py
@@ -0,0 +1,99 @@
+import os.path
+from typing import List
+
+import requests
+
+
+def get_input_file_name(year: int, day: int) -> str:
+	"""
+	Return the name on disk of the input file for the specified day and year.
+	"""
+	
+	return "y{year}/inputs/day{day:02d}.txt".format(
+		year=year,
+		day=day,
+	)
+
+def download_input_from_server(year: int, day:int) -> str:
+	"""
+	Download the specified input file from the server, and save it to disk for
+		next time.
+	
+	Returns: The full contents of the input file.
+	"""
+	
+	input_file_name = get_input_file_name(year, day)
+	
+	# Get the saved session cookie.
+	try:
+		with open(".session") as file:
+			session = file.read().strip()
+	
+	# If we can't find a saved session cookie, raise an exception.
+	except FileNotFoundError as ex:
+		print("Error: No saved input or .session cookie found.")
+		raise ex
+	
+	r = requests.get(
+		f"https://adventofcode.com/{year}/day/{day}/input",
+		cookies={"session": session},
+	)
+	
+	# If the server responded with a bad status code, raise an exception.
+	if r.status_code != 200:
+		raise Exception(
+			"AoC server responded with bad status code when attempting to "
+			f"retrieve input file: {r.status_code}"
+		)
+		
+	# Otherwise, we're all good.
+	input_contents = r.text
+	
+	# Save the retrieved input for next time.
+	with open(input_file_name, "w") as file:
+		file.write(input_contents.strip())
+	
+	return input_contents
+
+def retrieve_input(year: int, day: int) -> List[str]:
+	"""
+	Get the input file for the day and year's challenge.
+	
+	If the specified input file is saved on disk, read it from there. Otherwise,
+		download it from the AoC site, and save it to the disk for later use.
+	
+	Args:
+		year: The year of the puzzle to retrieve the input for.
+		day: The day of the challenge to retrieve the input for.
+	
+	Returns: A list of the lines of the input file.
+	"""
+	
+	input_file_name = get_input_file_name(year, day)
+	input_file_dir = os.path.dirname(input_file_name)
+	
+	if not os.path.isdir(input_file_dir):
+		os.makedirs(input_file_dir)
+	
+	# First, try reading from a saved file.
+	try:
+		with open(input_file_name) as file:
+			input_contents = file.read()
+	
+	# If that doesn't work, download it from the AoC site.
+	except FileNotFoundError:
+		print("No input file found; retrieving input from AoC site.")
+		
+		input_contents = download_input_from_server(year, day)
+	
+	return input_contents.splitlines()
+
+def retrieve_test_input() -> List[str]:
+	"""
+	Get the test input located in test_input.txt.
+	
+	Returns: A list of the lines of the test input.
+	"""
+	
+	with open("test_input.txt") as file:
+		return file.readlines()
--- solve_day.py
+++ solve_day.py
@@ -7,59 +7,58 @@ import sys
 
 import requests
 
+from input_retrieval import retrieve_input, retrieve_test_input
+
 def dname(day):
 	return "day{:02d}".format(day)
 
 def yname(year):
 	return "y{}".format(year)
 
-def retrieve_input(year, day):
-	file_dir = "{}/inputs/".format(yname(year))
-	file_name = file_dir + "{}.txt".format(dname(day))
-	if not os.path.isdir(file_dir):
-		os.makedirs(file_dir)
-	
-	try:
-		with open(file_name) as file:
-			input_file = file.read()
-	
-	except FileNotFoundError:
-		print("No input file found; retrieving input remotely.")
-		try:
-			with open(".session") as file:
-				sess = file.read().strip()
-
-		except FileNotFoundError:
-			print("Error: Neither saved input file nor .session cookie file could be found.")
-			sys.exit(1)
-		
-		r = requests.get("https://adventofcode.com/{}/day/{}/input".format(year, day),
-							cookies={"session": sess})
-
-		if r.status_code != 200:
-			print("Error: Unable to retrieve input file.")
-			sys.exit(1)
-		
-		input_file = r.text
-
-		with open(file_name, "w") as file:
-			file.write(input_file.strip())
-
-	return input_file.splitlines()
-
 if __name__ == "__main__":
 	
-	parser = argparse.ArgumentParser(description="Solve a day of the advent of code.")
-	parser.add_argument("day", type=int,
-						help="which day to solve the problem for")
-	parser.add_argument("-t", "--test", dest="testing",
-						action="store_const", const=True, default=False,
-						help="read input from the test file .test")
-	parser.add_argument("--part", "-p", dest="part", default=None, choices=("1", "2"),
-						help="the particular part to solve, instead of both")
-	years = [dirname[1:] for dirname in os.listdir(".") if dirname.startswith("y")]
-	parser.add_argument("--year", "-y", dest="year", default=max(years), choices=years,
-						help="which year to solve the day for")
+	parser = argparse.ArgumentParser(
+		description=(
+			"Run the solver for a given day of the Advent of Code challenge."
+		)
+	)
+	parser.add_argument(
+		"day",
+		type=int,
+		help="which day to solve the problem for"
+	)
+	parser.add_argument(
+		"--test",
+		"-t",
+		dest="testing",
+		action="store_const",
+		const=True,
+		default=False,
+		help="use test input (from .test file) instead of real input"
+	)
+	parser.add_argument(
+		"--part",
+		"-p",
+		dest="part",
+		default=None,
+		choices=("1", "2"),
+		help="the particular part to solve; defaults to both"
+	)
+	
+	known_years = [
+		int(dirname[1:])
+		for dirname in os.listdir(".")
+		if dirname.startswith("y")
+	]
+	parser.add_argument(
+		"--year",
+		"-y",
+		dest="year",
+		default=max(known_years),
+		type=int,
+		choices=known_years,
+		help="the particular year to solve for; defaults to latest"
+	)
 	
 	args = parser.parse_args()
 	
@@ -68,27 +67,38 @@ if __name__ == "__main__":
 		sys.exit(1)
 	
 	try:
-		day_mod = import_module("{}.{}".format(yname(args.year), dname(args.day)))
+		solver_module = import_module(
+			"y{year}.day{day:02d}".format(
+				year=args.year,
+				day=args.day,
+			)
+		)
 	
 	except ImportError:
-		print("Error: No solution module found for {}-{}.".format(yname(args.year), dname(args.day)))
-		sys.exit(1)
+		raise Exception(f"No solver found for year {args.year}, day {args.day}")
 	
 	if args.testing:
-		with open(".test") as file:
-			lines = file.read().splitlines()
+		solver_input = retrieve_test_input()
 	
 	else:
-		lines = retrieve_input(args.year, args.day)
+		solver_input = retrieve_input(args.year, args.day)
 	
 	parts_to_eval = ("1", "2") if args.part is None else (args.part,)
 
-	if hasattr(day_mod, "parse_input"):
-		lines = day_mod.parse_input(lines)
+	# If the solver module has a specification for input formatting, run it.
+	if hasattr(solver_module, "parse_input"):
+		solver_input = solver_module.parse_input(solver_input)
+	
 	
 	for part in parts_to_eval:
 		part_name = "part" + part
-		if not hasattr(day_mod, part_name):
-			print("Error: Solution for year-{} day-{} has no function {}.".format(args.year, args.day, part_name))
+		if not hasattr(solver_module, part_name):
+			print(
+				f"Error: Solution for year {args.year}, day {args.day} has no "
+				f"function {part_name}."
+			)
 		else:
-			print("Part {} answer: {}".format(part, getattr(day_mod, part_name)(lines)))
+			part_solver = getattr(solver_module, part_name)
+			part_solution = part_solver(solver_input)
+			
+			print(f"Part {part} solution: {part_solution}")
