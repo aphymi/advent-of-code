3397c481452ec848a9a3404999d7020c6d0164ae
aphymi <aphymi@gmail.com>
2021-12-30 12:24:53 -0500

    refactor: adjust TileMap methods

--- util/tile_map.py
+++ util/tile_map.py
@@ -18,6 +18,13 @@ class TileMap(Generic[T]):
 		"""
 
 		return self.state[y][x]
+	
+	def set_at(self, x: int, y: int, new_value: T) -> None:
+		"""
+		Set the given value at the given x and y coordinates.
+		"""
+
+		self.state[y][x] = new_value
 
 	def walk(self) -> Generator[TileData, None, None]:
 		"""
@@ -30,7 +37,11 @@ class TileMap(Generic[T]):
 			for x, value in enumerate(row):
 				yield (value, x, y)
 	
-	def contains_coords(self, x: int, y: int) -> bool:
+	def contains_coordinates(self, x: int, y: int) -> bool:
+		"""
+		Return whether the given coordinates are within the map.
+		"""
+
 		return (
 			y in range(len(self.state))
 			and x in range(len(self.state[y]))
@@ -57,11 +68,33 @@ class TileMap(Generic[T]):
 			adj_x = x + modifier[0]
 			adj_y = y + modifier[1]
 
-			if not self.contains_coords(adj_x, adj_y):
+			if not self.contains_coordinates(adj_x, adj_y):
 				continue
 				
 			yield (
-				self.state[adj_y][adj_x],
+				self.get_at(adj_x, adj_y),
 				adj_x,
-				adj_y
+				adj_y,
 			)
+	
+	def get_adjacent_8(self, x: int, y: int) -> Generator[TileData, None, None]:
+		"""
+		Generate an enumeration of the tiles adjacent to x and y.
+		"""
+
+		for y_mod in (-1, 0, 1):
+			for x_mod in (-1, 0, 1):
+				if x_mod == y_mod == 0:
+					continue
+				
+				adj_x = x + x_mod
+				adj_y = y + y_mod
+
+				if not self.contains_coordinates(adj_x, adj_y):
+					continue
+
+				yield (
+					self.get_at(adj_x, adj_y),
+					adj_x,
+					adj_y,
+				)
